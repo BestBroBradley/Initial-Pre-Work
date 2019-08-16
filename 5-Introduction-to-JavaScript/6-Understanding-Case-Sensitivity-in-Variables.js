598df10096de3d2675cddfaa27315730b68a6126
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 17:50:41 -0600

    Adds lesson 6 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// I learned that best practice is to declare variables in camelCase, first word lowercase, first letter of each following word caps.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
