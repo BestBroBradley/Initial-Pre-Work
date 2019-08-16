6016c053ca2a892713034cf4f2c7bef0e1e5c7e3
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 17:27:56 -0600

    Adds lesson 2 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,8 @@
+/* I learned how to name a variable using "var".  I also learned that JavaScript uses seven different data types.
+undefined, null, boolean, string, symbol, number and object */
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
