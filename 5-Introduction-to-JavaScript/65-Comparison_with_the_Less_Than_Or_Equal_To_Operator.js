42ae425050eccd5aec97afd7e0f6d27a41b398dd
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:15:35 -0600

    Adds lesson 65 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+// I learned you can also use <= (less than or equal to) to compare your variable to a value.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
