12055a6d212dc2ebcb4e1ce68973f3a6242a0fd5
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:43:18 -0600

    Adds lesson 25 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// I learned you can add two strings together by using the + action
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
