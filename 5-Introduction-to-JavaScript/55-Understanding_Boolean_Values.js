bc16b46b317f78cf7b428cefb8ef03054eaf63f6
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 15:46:24 -0600

    Adds lesson 55 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// I leanred that a boolean is either true or false.  True is considered "on", while false is "off".
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
