1671a4b8e1e38ee74ad734b0a2a44ff070022d1e
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:07:27 -0600

    Adds lesson 61 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// I learned you can use !== to return true only if the elements are not equal, including data type.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
