264e41e318c2aa5cdfae08c2bf100ffaad2790e5
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:57:25 -0600

    Adds lesson 30 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// I learned that JavaScript starts counting from 0 instead of 1, and that you can find the value of the first character in a string using []
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
