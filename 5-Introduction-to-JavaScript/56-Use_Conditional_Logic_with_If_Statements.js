480d79f8227c3211e41106fcfaf2a02688f5cefd
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 15:54:11 -0600

    Adds lesson 56 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,25 @@
+// I learned that you can use if to execute functions only if certain criteria are met.
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue) {
+      return "Yes, that was true";
+  }
+  return "No, that was false";
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
