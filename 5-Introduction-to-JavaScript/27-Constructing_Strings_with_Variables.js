2c88829312b1b9c217fde539580a8c94311766bc
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:49:57 -0600

    Adds lesson 27 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// I learned that you can use + to add additional variables into the middle of multiple strings.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Inigo Montoya";
+var myStr = "My name is " + myName + " and I am well!";
