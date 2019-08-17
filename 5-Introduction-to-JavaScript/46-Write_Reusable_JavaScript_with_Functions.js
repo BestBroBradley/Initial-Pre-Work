79158838d669e9e0713db88666907d2be0a1e9b1
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 13:01:07 -0600

    Adds lesson 46 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// I learned how to create a function which you can execute any number of times later in the code and how to execute it.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
