361edbc15e346d798c225b1d75f4620345cde962
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 10:18:05 -0600

    Adds lesson 31 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// I learned that you can't change the values inside a string literal, you have to actually replace that string literal instead.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
