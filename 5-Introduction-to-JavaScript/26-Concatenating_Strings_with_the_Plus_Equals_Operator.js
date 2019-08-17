33873bd217e285f800ec052355186b73f4a8c03d
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:46:01 -0600

    Adds lesson 26 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// I learned that you can use the += action as shorthand to add two strings together.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
