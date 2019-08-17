a283cd4555f5e7e6bbf90c85ff80ef888e1eaa9d
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:12:34 -0600

    Adds lesson 63 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+// I learned you can use >= (greater than or equal to) to compare your variable to a value.
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10); 
