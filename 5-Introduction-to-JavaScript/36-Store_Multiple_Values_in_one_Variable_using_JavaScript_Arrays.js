d385b4c35f9c4c3d821c3f0b237b130a9b6999c9
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 11:20:46 -0600

    Adds lesson 36 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// I learned that you can apply multiple packages of data to one variable.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Cats rule, dogs drool.", 42];
