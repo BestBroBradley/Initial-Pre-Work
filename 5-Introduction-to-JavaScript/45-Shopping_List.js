5676af2e40286a376f77a9fe19bcdfb3bc6e97a3
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 12:30:06 -0600

    Adds lesson 45 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,10 @@
+// I got practice compiling a multi-dimensional array.
+
+var myList = [
+    ["Vulcan coffee mug",5],
+    ["Captain Kirk T-shirt",1],
+    ["Klingon battle axe",8],
+    ["Borg headgear",15],
+    ["Enterprise Model Kits",3]
+
+];
