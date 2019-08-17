2bdb29e38f2cfabe7d030505ea41980d78188a95
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 10:24:22 -0600

    Adds lesson 32 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// I learned that you can use brackets [] to assign the nth character of a variable to another variable.  Note: counting starts at 0.
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
