a73d5fbfe716426c226fd56c48d865dbf3e2bb3a
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 14:47:48 -0600

    Adds lesson 51 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+// I learned that you can use return to obtain a value from a function.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5
+}
+var answer = timesFive(5);
+
+console.log(minusSeven(10));
