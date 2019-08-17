6e6a5aae55b211a5b4a4f04536ab347c69a806f2
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:27:43 -0600

    Adds lesson 68 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,20 @@
+// I learned that you can provide a code to execute in the event of a "false" return using "else"
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
