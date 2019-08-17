5dad08c98e71b6d04ceddba34f04e56ca2862105
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:52:33 -0600

    Adds lesson 28 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// I learned that you can use the += to append some previously named variable to a string.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "super special awesome!";
+var myStr = "Learning to code is ";
+myStr += someAdjective
