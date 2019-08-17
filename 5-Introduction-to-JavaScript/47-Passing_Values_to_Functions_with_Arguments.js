e97803d31f67f125ca2aab6498e7345eea12b095
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 13:11:35 -0600

    Adds lesson 47 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+// I learned that you can assign parameters (essentially stand-in values) and how to change them upon execution.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+functionWithArgs(12,14);
