7b65e0dc834ee484b052fc26e714284b8cc2dda0
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:39:55 -0600

    Adds lesson 70 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+// I learned that the order of "else if" statements is important because they are read from top to bottom.
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
