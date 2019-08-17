5ce0cd403b691105b36a87104bb3f9b45b1b10c4
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 11:53:33 -0600

    Adds lesson 38 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// I learned that you can also use brackets to dicate which entry in an array you want to access.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+// Only change code below this line.
