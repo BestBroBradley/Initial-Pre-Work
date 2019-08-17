c12253b5924602afc1e99794739653ae9dc9098e
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 12:22:19 -0600

    Adds lesson 43 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,12 @@
+// I learned that you can use .shift() to remove the first value from an array and assign it elsewhere.
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
