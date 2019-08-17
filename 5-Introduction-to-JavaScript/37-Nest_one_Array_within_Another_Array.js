6b834a0e155bff3e6536c977293b350a81a7fa49
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 11:46:43 -0600

    Adds lesson 37 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// I learned that you can combine arrays to nest an array within another array in a variable.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Cats rule", "1989"], ["Dogs drool", "2001"]];
