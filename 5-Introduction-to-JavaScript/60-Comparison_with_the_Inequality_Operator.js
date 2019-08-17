c756aacd2e94541298edfd01b1ba118b23436538
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 16:05:45 -0600

    Adds lesson 60 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// I learned that != will return true only if the elements are not equal to the given amount.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
