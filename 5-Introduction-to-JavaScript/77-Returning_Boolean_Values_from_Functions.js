d10371eb6f0863becceaed9d6fd63223641ca4ce
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 17:31:47 -0600

    Adds lesson 77 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// I learned that you can replace some else if statements with comparison operators
+
+function isLess(a,b) {
+  // Fix this code
+  return (a < b);
+}
+
+// Change these values to test
+isLess(10, 15);
