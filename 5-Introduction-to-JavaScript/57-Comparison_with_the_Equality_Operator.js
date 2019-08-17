4e9d0afc94c934271abf803157329cae22401f9c
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 15:58:47 -0600

    Adds lesson 57 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// I learned that you can use == to execute an "if" statement only if the two designated elements are equivalent.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
