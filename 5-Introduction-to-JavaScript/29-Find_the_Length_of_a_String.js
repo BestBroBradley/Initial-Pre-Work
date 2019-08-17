6e9c489f020e2dbe42f5025137a134b573ef48f6
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:55:11 -0600

    Adds lesson 29 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+// I learned you can ask the code for the length of a variable by using .length
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
