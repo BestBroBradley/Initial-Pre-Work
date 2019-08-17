521f40d81e4ed4d5cd81a175e3d29a8f3c393ba6
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:30:05 -0600

    Adds lesson 22 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// I learned you can use \"\" to denote quotes you want Java to read as actual quotes and not as the beginning or end of a string.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
