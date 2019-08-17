c8ea9ea7db8bd05e5bfaa30fa4d79d594303701d
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:38:59 -0600

    Adds lesson 24 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+// I learned that you can use \ as part of an escape sequence to do a variety of actions like adding a tab or going to a new line.
+
+var myStr = "\nFirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
