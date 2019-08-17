5272c72bed12909d1a78c3dde7dbbee734071032
Allison Bradley <allison.bradley@outlook.com>
2019-08-16 18:26:18 -0600

    Adds lesson 21 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// I learned that you can use a string (a series of characters bracketed by "") as a variable.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Allison"
+var myLastName = "Bradley"
