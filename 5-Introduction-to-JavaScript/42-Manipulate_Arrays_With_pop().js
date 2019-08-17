3b2a24086d62ee07f41d47935bbc2b9cdec47a5d
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 12:18:48 -0600

    Adds lesson 42 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+// I learned that you can remove the last value from an array and add it somewhere else with .pop()
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop();
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
