0f8b6c382d91c1eed393b69e9db24c9137b73da7
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 19:26:50 -0600

    Adds lesson 82 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// I learned that you can use bracket notation instead of dot notation to retreive property traits
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
