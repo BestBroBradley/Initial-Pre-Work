bfc5d91cce863c8f818ed3735aaed024054e17f0
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 13:07:20 -0600

    Adds lesson 99 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+// I learned that in a while loop, the condition is checked first.  In a "do...while" loop, the condition is checked after an initial action code happens
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 5);
