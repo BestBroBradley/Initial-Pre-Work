1f7923e367fa132cc3a12adc25906c1989d4e5df
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 15:28:54 -0600

    Adds lesson 2 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+// I learned that let also reduces the scope in which the given variable keeps that value
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
