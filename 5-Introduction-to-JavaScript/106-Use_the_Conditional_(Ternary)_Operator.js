e5a139503c61382a392dea6f15794affbb3c0a8e
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 15:05:16 -0600

    Adds Lesson 106 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+//  learned that you can use the conditional operator as shorthand to replace if else statements
+
+function checkEqual(a, b) {
+    return a === b ? true : false;
+}
+
+checkEqual(1, 2);
