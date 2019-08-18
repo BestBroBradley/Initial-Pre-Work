07a235341064bc19d59a2b99fbf4da19d9220646
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 12:06:13 -0600

    Adds lesson 93 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+// I learned that you can use "while" to create a code which will repeatedly run as long as certain criteria are true
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i<5) {
+  myArray.push(i);
+  i++;
+}
