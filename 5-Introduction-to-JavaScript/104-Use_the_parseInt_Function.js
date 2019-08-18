de0156491e9253e2417c520b271f80820d7acbb9
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 14:53:46 -0600

    Adds lesson 104 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+// I learned that you can use parseInt to analyze a string and return it as either an integer or NaN (not a number)
+
+function convertToInteger(str) {
+ return parseInt(str);
+}
+
+convertToInteger("56");
