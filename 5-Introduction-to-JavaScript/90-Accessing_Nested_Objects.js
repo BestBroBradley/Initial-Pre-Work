2e2c2c923e68f30fe51de7a07d79009b2d3feb11
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 22:01:23 -0600

    Adds lesson 90 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// I learned that you can have objects inside other objects, and you can access them with dot and bracket notation
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
