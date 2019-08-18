c71cbfcb4b81d4475e716764a51ed9beea3db9d8
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 09:30:19 -0600

    Adds lesson 91 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+// I learned that you can also use dot and bracket notation to access an array within an array
+
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
