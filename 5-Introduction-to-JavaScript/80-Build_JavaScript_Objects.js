c452506f6780891eca38e64e3b71cdfacdba219f
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 19:12:50 -0600

    Adds lesson 80 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// I learned that you can create an object, similar to an array, which is a collection of "properties"
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name" : "Chairman Meow",
+  "legs" : 8,
+  "tails" : 2,
+  "friends" : ["Vladimir Purrtin", "Meowssolini"]
+};
