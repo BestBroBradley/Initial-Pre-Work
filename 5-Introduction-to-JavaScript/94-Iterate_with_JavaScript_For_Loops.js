9636409cbd6c72d666ee5813c89aedc8d82a9d21
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 12:13:39 -0600

    Adds lesson 94 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// I learned that you can also use a "for" loop to loop a code until certain criteria are met
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i <= 5; i++) {
+  myArray.push(i);
+}
