f79e381771688d556f256e31dfdf9888792d742e
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 15:12:31 -0600

    Adds lesson 107 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,9 @@
+// I learned that you can use a conditional operator to replace multiple connected if else statements
+
+function checkSign(num) {
+
+ return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero";
+
+}
+
+checkSign(10);
