7da20de805f6cea6b4e7071477a9e50940d9fcc7
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 15:21:45 -0600

    Adds lesson 1 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+// I learned that you can use let instead of var to ensure each variable is named only once
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
