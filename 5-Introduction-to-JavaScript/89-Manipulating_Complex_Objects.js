95764ee55e249adedc85853303c14614f65a7666
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 21:52:03 -0600

    Adds lesson 89 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// I learned that an array can consist of multiple objects, and those objects can use a variety of different data types
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+{
+    "artist": "The Moomin Brothers",
+    "title": "Every Day I'm Moomin On",
+    "release_year": 1091,
+    "formats": [
+      "UFO",
+      "Smoke screens",
+      "Mime"
+    ]
+  }
+];
