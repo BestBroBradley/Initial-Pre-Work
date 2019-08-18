68ea660a59df394fb4645414493ea93db87a0e7f
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 20:28:29 -0600

    Adds lesson 83 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// I learned that you can use bracket notation to pull information from a dynamic property
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
