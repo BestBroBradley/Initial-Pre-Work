59da213f8088cd6fbe21e464ad08cab98d3e9160
Allison Bradley <allison.bradley@outlook.com>
2019-08-17 19:19:33 -0600

    Adds lesson 81 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+// I learned that you can use dot notation to access the value of a specific, explicit property
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
