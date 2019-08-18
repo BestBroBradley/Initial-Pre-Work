624020320a61429b0e32d0aee9c8ac12d8891ad8
Allison Bradley <allison.bradley@outlook.com>
2019-08-18 14:33:42 -0600

    Adds lesson 101 - Intro to Java

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// I learned that Math.random() will return a random fraction between 0 and 1
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
