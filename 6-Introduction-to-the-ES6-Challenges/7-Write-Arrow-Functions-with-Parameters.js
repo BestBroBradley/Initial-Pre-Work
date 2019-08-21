7b9e9255a6760256fb3a67e39d1357e4ce4bdb65
Allison Bradley <allison.bradley@outlook.com>
2019-08-20 19:24:43 -0600

    Adds lesson 7 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,5 @@
+// I learned that you can use the => arrow to complete more complex functions as well.
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2)
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
