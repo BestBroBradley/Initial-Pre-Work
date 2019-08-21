aba920a52a279013808e2cb051e2f89b3e08a908
Allison Bradley <allison.bradley@outlook.com>
2019-08-20 20:52:28 -0600

    Adds lesson 10 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,9 @@
+// I learned that you can use the rest operator (...) to allow a function to have an unspecified number of arguments.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
