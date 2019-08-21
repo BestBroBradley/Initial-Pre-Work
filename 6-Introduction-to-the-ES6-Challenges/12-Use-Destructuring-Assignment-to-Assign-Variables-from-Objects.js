b7580b5493bddcb3e5febf62b93e14ca7c88f0d9
Allison Bradley <allison.bradley@outlook.com>
2019-08-20 22:00:14 -0600

    Adds lesson 12 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+// I learned that you can use ES6 to shorten the way you define parameters
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow:tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
