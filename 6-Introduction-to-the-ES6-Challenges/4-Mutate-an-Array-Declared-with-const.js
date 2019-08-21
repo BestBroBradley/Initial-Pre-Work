6f32d1cb64edf403e7cbb1be34054f4ae2d1980a
Allison Bradley <allison.bradley@outlook.com>
2019-08-20 18:53:51 -0600

    Adds lesson 4 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
@@ -0,0 +1,14 @@
+// I learned that const variables can still have their value changed, just not their designation.
+
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  // s = [2, 5, 7]; <- this is invalid
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+  // change code above this line
+}
+editInPlace();
