ee1f9c59084f817be0725d31e69752520ddb8605
Allison Bradley <allison.bradley@outlook.com>
2019-08-20 20:25:46 -0600

    Adds lesson 9 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+// I learned that you can set a default value for parameters to use when they are undefined
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
