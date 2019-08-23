d96c049aff6f3eae9859cfd94a5a0d8de67946ea
Allison Bradley <allison.bradley@outlook.com>
2019-08-22 18:19:08 -0600

    Adds lesson 22 - Java ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// I learned that you can use the import function to pull variables and objects from a separate file.
+
+"use strict";
+import { capitalizeString } from "string_functions";
+capitalizeString("hello!");
